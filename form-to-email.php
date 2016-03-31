ad5a8f9fcbd8c11e801d3232cc8061a9a32087a5
sagiab <sagi10ab@gmail.com>
2016-03-31 16:20:16 +0300

    Update form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -21,7 +21,7 @@ if(IsInjected($visitor_email))
     exit;
 }
 
-$email_from = 'tom@amazing-designs.com';//<== update the email address
+$email_from = 'sagi10ab@gmail.com';//<== update the email address
 $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
